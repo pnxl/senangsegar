8250543dec44a216138414bd52778c9144263502
Jason <development@pnxl.dev>
2024-04-22 19:11:59 +0700

    feat: start survey page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div class="min-h-screen p-8 md:w-[56rem]">
+    <div class="min-w-full flex justify-center">
+      <button
+        class="rounded-full bg-brand-darkest w-full hover:bg-brand-darker p-4 md:w-96 justify-between flex"
+      >
+        Start the survey!
+      </button>
+    </div>
+  </div>
 </template>
