a587c1bdc3f0d9c93875026e2b765a426e18906f
Jason <development@pnxl.dev>
2024-04-22 19:42:32 +0700

    styles: buton

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen p-8 md:w-[56rem]">
     <div class="min-w-full flex justify-center">
       <button
-        class="rounded-full bg-brand-darkest w-full hover:bg-brand-darker p-4 md:w-96 justify-between flex"
+        class="rounded-full dark:bg-brand-darkest text-brand-darkest font-semibold bg-brand-light shadow-brand-darker shadow-sm w-full hover:bg-brand-dark dark:hover:bg-brand-darker p-4 md:w-96 justify-between flex"
       >
         Start the survey!
       </button>
