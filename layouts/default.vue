f7d0fe2b93bae2c537fa00a929fa48c00e1ca352
Jason <development@pnxl.dev>
2024-04-22 19:40:53 +0700

    styles: just use a white background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen md:min-w-full md:flex md:justify-center text-sm bg-brand-lightest text-brand-darker dark:bg-neutral-900 dark:text-brand-light"
+    class="min-h-screen md:min-w-full md:flex md:justify-center text-sm bg-neutral-100 text-brand-darker dark:bg-neutral-900 dark:text-brand-light"
   >
     <slot />
   </div>
