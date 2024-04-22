944bad32cf3f63216e017bf79f415a87d7858314
Jason <development@pnxl.dev>
2024-04-22 13:07:20 +0700

    styles: use brand colours

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,6 @@
 <template>
   <div
-    class="
-      min-h-screen min-w-full
-      bg-gray-100
-      text-gray-800
-      dark:bg-gray-900
-      dark:text-gray-200
-    "
+    class="min-h-screen min-w-full bg-brand-lightest text-brand-darker dark:bg-brand-darkest dark:text-brand-light"
   >
     <slot />
   </div>
