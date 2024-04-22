8df6f82e72875ceb802b48ca565f50cb9009125c
Jason <development@pnxl.dev>
2024-04-22 22:28:57 +0700

    feat: start homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div
+    class="min-h-screen md:py-16 md:px-24 lg:px-32 2xl:px-48 sm:p-12 p-8 min-w-full flex flex-col gap-y-4"
+  >
+    <h1
+      class="font-display text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium w-1/2"
+    >
+      Melayani pengalaman yang menyenangkan.
+    </h1>
+  </div>
 </template>
