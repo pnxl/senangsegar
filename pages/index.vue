fb44f6c38894752e5632bc2c341572ca7d9c2a8d
Jason <development@pnxl.dev>
2024-04-23 22:30:43 +0700

    feat: continue homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="min-h-screen min-w-full flex flex-col gap-y-4">
-    <div>
+    <div class="flex flex-col gap-y-8">
       <h1
-        class="font-display text-3xl mt-16 sm:text-4xl md:text-5xl lg:text-6xl font-medium w-1/2"
+        class="font-display text-3xl mt-16 sm:text-4xl md:text-5xl lg:text-6xl font-medium w-1/2 dark:text-brand-light"
       >
         Melayani pengalaman yang menyenangkan.
       </h1>
-      <p class="text-lg w-1/2">
+      <p class="text-lg w-1/2 dark:text-brand-dark">
         Kami menyediakan makanan lezat beraroma, dibuat hanya dengan bahan-bahan
         segar.
       </p>
+      <nuxt-link
+        to="/menu"
+        class="dark:bg-brand-darkest bg-brand-light dark:text-brand-light dark:hover:bg-brand-darker dark:hover:text-brand-lighter hover:bg-brand-dark hover:text-brand-darkest py-4 px-12 w-fit rounded-full font-medium"
+      >
+        <p>Lihat Menu</p>
+      </nuxt-link>
     </div>
   </div>
 </template>
