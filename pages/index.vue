66c6b45f541eea90da7547dc7e0d9fd8148b3ff9
Jason <development@pnxl.dev>
2024-04-23 16:54:05 +0700

    feat: add subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="min-h-screen min-w-full flex flex-col gap-y-4">
-    <h1
-      class="font-display text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium w-1/2"
-    >
-      Melayani pengalaman yang menyenangkan.
-    </h1>
+    <div>
+      <h1
+        class="font-display text-3xl mt-16 sm:text-4xl md:text-5xl lg:text-6xl font-medium w-1/2"
+      >
+        Melayani pengalaman yang menyenangkan.
+      </h1>
+      <p class="text-lg w-1/2">
+        Kami menyediakan makanan lezat beraroma, dibuat hanya dengan bahan-bahan
+        segar.
+      </p>
+    </div>
   </div>
 </template>
