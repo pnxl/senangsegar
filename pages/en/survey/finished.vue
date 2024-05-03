64cbd6652fd7789ca3e5a75aabc4e9265dca51df
Jason <development@pnxl.dev>
2024-05-03 21:10:22 +0700

    styles: add icon to english finished

--- pages/en/survey/finished.vue
+++ pages/en/survey/finished.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen min-w-full flex flex-col gap-y-4">
+  <div class="min-w-full flex flex-row gap-x-8 justify-between">
     <div class="flex flex-col gap-y-8">
       <h1
         class="font-display text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium md:w-1/2 dark:text-brand-light"
@@ -15,8 +15,11 @@
         to="/en/"
         class="dark:bg-brand-darkest bg-brand-lighter dark:text-brand-light dark:hover:bg-brand-darker dark:hover:text-brand-lighter hover:bg-brand-light hover:text-brand-darkest py-4 px-12 w-fit rounded-full font-medium"
       >
-        <p>Go Back Home</p>
+        <p>Go Back</p>
       </nuxt-link>
+    </div>
+    <div>
+      <img src="@/assets/logo.png" class="w-96 hidden sm:block" />
     </div>
   </div>
 </template>
