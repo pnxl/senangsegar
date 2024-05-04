d136f311ec19b9848f48f0afb01a71e087632927
Jason <development@pnxl.dev>
2024-05-04 16:50:23 +0700

    styles: change page transition

--- app.vue
+++ app.vue
@@ -9,12 +9,12 @@
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+  transition: all 0.3s;
 }
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  filter: blur(1rem);
+  scale: 0.95;
 }
 
 .layout-enter-active,
