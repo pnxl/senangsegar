29c6e121c9ef20cce61d9fdaec3fe35056a77e32
Jason <development@pnxl.dev>
2024-05-04 15:09:10 +0700

    chore: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,47 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     layoutTransition: { name: "layout", mode: "out-in" },
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "msapplication-TileColor",
+          content: "#d9ffd7",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
+      ],
+      link: [
+        // massive shoutout https://realfavicongenerator.net/ - we love you 💖
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+        {
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#90c38b",
+        },
+      ],
+      noscript: [{ children: "JavaScript is required" }],
+    },
   },
   runtimeConfig: {
     databasePassword: "",
