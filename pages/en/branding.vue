835a060f69ce4323579a3ef9abc5119db5e793bf
Jason <development@pnxl.dev>
2024-05-05 12:50:37 +0700

    feat: add branding pages

---
+++ pages/en/branding.vue
@@ -0,0 +1,255 @@
+<template>
+  <div class="min-h-screen min-w-full flex flex-col gap-y-4">
+    <div class="flex flex-col gap-y-8">
+      <div class="flex flex-col gap-y-4">
+        <h1
+          class="font-display text-3xl md:text-4xl lg:text-5xl font-medium dark:text-brand-light"
+        >
+          Branding
+        </h1>
+        <p>
+          The Senang Segar brand belongs to Fang Industries, inc., the company
+          that guides and supports development of Senang Segar. These guidelines
+          describe when and how the elementary brand should be used.
+        </p>
+
+        <p>
+          You may not use the Senang Segar name in any way that could mistakenly
+          imply any official connection with or endorsement of catto labs. Any
+          use of the Senang Segar name in a manner that could cause customer
+          confusion is not permitted.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Brand Assets</h1>
+
+        <p>
+          Senang Segar claims two marks: the default character logomark, aswell
+          as it's expression variants. Both are considered trademarks and
+          represent Senang Segar.
+        </p>
+
+        <p>
+          These assets are provided for use in situations like articles, video
+          tutorials, or for featuring on your website. They should not be used
+          in any way that could be confusing for customers or imply any
+          affiliation with Senang Segar.
+        </p>
+
+        <p>All should be used with the following in mind:</p>
+        <ul class="list-disc gap-y-1 flex flex-col" style="margin-left: 1.5rem">
+          <li>
+            Do not stretch, skew, rotate, flip, or otherwise alter the marks.
+          </li>
+          <li>
+            Do not use the marks on an overly-busy background; solid colors work
+            best.
+          </li>
+          <li>The marks should always retain it's original colour.</li>
+        </ul>
+
+        <div class="flex md:flex-row flex-col gap-y-4 gap-x-4">
+          <div
+            class="p-6 sm:p-6 md:p-8 border-2 aspect-square relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/logo.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+          <div
+            class="p-6 sm:p-6 md:p-8 border-2 aspect-square relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/logo.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+          <div
+            class="p-6 aspect-square sm:p-6 md:p-8 border-2 relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/sad.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-sad-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-sad.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Colour</h1>
+
+        <p>
+          We employ the use of color combined with our name and marks to
+          establish our brand. We use the following palette:
+        </p>
+
+        <div class="grid md:grid-cols-3 grid-cols-2 gap-y-4 gap-x-4">
+          <div
+            class="flex xl:flex-row text-center xl:text-left flex-col justify-between p-4 border-2 border-brand-lightest bg-brand-darkest rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lightest my-auto">Palm</p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lightest opacity-75">HEX: #324930</p>
+              <p class="text-brand-lightest opacity-75">RGB: 50, 73, 48</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-lighter bg-brand-darker rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lighter my-auto">
+              Glade Green
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lighter opacity-75">HEX: #577B53</p>
+              <p class="text-brand-lighter opacity-75">RGB: 87, 123, 83</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-lighter bg-brand-dark rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lighter my-auto">
+              Dark Sea Green
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lighter opacity-75">HEX: #90C38B</p>
+              <p class="text-brand-lighter opacity-75">RGB: 144, 195, 139</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darker bg-brand-light rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darker my-auto">
+              Tea Green
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darker opacity-75">HEX: #BAF1B5</p>
+              <p class="text-brand-darker opacity-75">RGB: 186, 241, 181</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darker bg-brand-lighter rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darker my-auto">
+              Honeydew
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darker opacity-75">HEX: #D9FFD7</p>
+              <p class="text-brand-darker opacity-75">RGB: 204, 255, 201</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darkest bg-brand-lightest rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darkest my-auto">
+              Snowy Mint
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darkest opacity-75">HEX: #EBFFEA</p>
+              <p class="text-brand-darkest opacity-75">RGB: 217, 255, 215</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Font</h1>
+
+        <p>
+          For web dan print, we use Grandstander for titles, dan Montserrat for
+          body text.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Merchandise</h1>
+
+        <p>
+          We do not authorise the use of our trademarks for t-shirts, stickers,
+          or other merchandise without explicit written consent.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4 mb-16">
+        <h1 class="text-2xl font-medium dark:text-brand-light">
+          Additional Information
+        </h1>
+
+        <p>
+          Our official branding assets are licensed under
+          <nuxt-link
+            class="hover:underline dark:hover:text-brand-lightest hover:text-brand-darkest"
+            to="/id/redirect?url=https://github.com/pnxl/senangsegar/blob/main/LICENSE"
+            >Creative Commons BY-NC-ND 4.0</nuxt-link
+          >.
+        </p>
+
+        <p>
+          For further information regarding the use of the elementary name,
+          branding, and trademarks, please email
+          <a
+            class="hover:underline dark:hover:text-brand-lightest hover:text-brand-darkest"
+            href="mailto:brand@senangsegar.xyz"
+            >brand@senangsegar.xyz</a
+          >.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "english",
+});
+</script>
---
+++ pages/id/branding.vue
@@ -0,0 +1,252 @@
+<template>
+  <div class="min-h-screen min-w-full flex flex-col gap-y-4">
+    <div class="flex flex-col gap-y-8">
+      <div class="flex flex-col gap-y-4">
+        <h1
+          class="font-display text-3xl md:text-4xl lg:text-5xl font-medium dark:text-brand-light"
+        >
+          Merek
+        </h1>
+        <p>
+          Merek Senang Segar adalah milik Fang Industries, inc., perusahaan yang
+          memandu dan mendukung pengembangan Senang Segar. Pedoman ini
+          menjelaskan kapan dan bagaimana merek dasar harus digunakan.
+        </p>
+
+        <p>
+          Anda tidak boleh menggunakan nama Senang Segar dengan cara apa pun
+          yang dapat secara keliru menyiratkan hubungan resmi atau dukungan
+          terhadap Senang Segar. Anda dilarang menggunakan nama Senang Segar
+          dengan cara yang dapat menyebabkan kebingungan pelanggan.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Aset Merek</h1>
+
+        <p>
+          Senang Segar mengklaim tiga lambang: tanda lambang maskot biasa, serta
+          varian ekspresinya. Keduanya dianggap merek kami dan mewakili Senang
+          Segar.
+        </p>
+
+        <p>
+          Lambang ini disediakan untuk digunakan dalam situasi seperti artikel,
+          video tutorial, atau untuk ditampilkan di situs web Anda. Lambang ini
+          tidak boleh digunakan dengan cara apa pun yang dapat membingungkan
+          pelanggan atau menyiratkan afiliasi apa pun dengan Senang Segar.
+        </p>
+
+        <p>Ketiganya harus digunakan dengan mengingat hal berikut:</p>
+        <ul class="list-disc gap-y-1 flex flex-col" style="margin-left: 1.5rem">
+          <li>
+            Jangan meregangkan, memiringkan, memutar, membalik, atau mengubah
+            lambangnya.
+          </li>
+          <li>
+            Jangan gunakan lambang pada latar belakang yang terlalu sibuk; warna
+            solid bekerja paling baik.
+          </li>
+          <li>Lambang tersebut harus selalu mempertahankan warna aslinya.</li>
+        </ul>
+
+        <div class="flex md:flex-row flex-col gap-y-4 gap-x-4">
+          <div
+            class="p-6 sm:p-6 md:p-8 border-2 aspect-square relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/logo.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+          <div
+            class="p-6 sm:p-6 md:p-8 border-2 aspect-square relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/logo.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-default.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+          <div
+            class="p-6 aspect-square sm:p-6 md:p-8 border-2 relative w-fit border-neutral-300 dark:border-neutral-700 bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/625950f8da1880cda1a94346_bg-w.png)] dark:bg-[url(https://assets-global.website-files.com/6257adef93867e50d84d30e2/62594f6abf11bb058da52ecb_bg.png)] rounded-xl"
+          >
+            <img
+              src="@/assets/branding/sad.png"
+              class="aspect-square lg:h-48 h-32"
+            />
+            <div class="absolute bottom-2 right-2 flex gap-x-3">
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-sad-1024.png"
+                download
+                >.png</a
+              >
+              <a
+                class="shadow-xl dark:hover:text-brand-lightest hover:text-brand-darkest"
+                href="/branding/senangsegar-mascot-sad.svg"
+                download
+                >.svg</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Warna</h1>
+
+        <p>
+          Kami menggunakan penggunaan warna yang dipadukan dengan nama dan merek
+          kami untuk membangun merek kami. Kami menggunakan palet berikut:
+        </p>
+
+        <div class="grid md:grid-cols-3 grid-cols-2 gap-y-4 gap-x-4">
+          <div
+            class="flex xl:flex-row text-center xl:text-left flex-col justify-between p-4 border-2 border-brand-lightest bg-brand-darkest rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lightest my-auto">
+              Palem
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lightest opacity-75">HEX: #324930</p>
+              <p class="text-brand-lightest opacity-75">RGB: 50, 73, 48</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-lighter bg-brand-darker rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lighter my-auto">
+              Hijau Rawa
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lighter opacity-75">HEX: #577B53</p>
+              <p class="text-brand-lighter opacity-75">RGB: 87, 123, 83</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-lighter bg-brand-dark rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-lighter my-auto">
+              Lautan Gelap
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-lighter opacity-75">HEX: #90C38B</p>
+              <p class="text-brand-lighter opacity-75">RGB: 144, 195, 139</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darker bg-brand-light rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darker my-auto">
+              Hijau Teh
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darker opacity-75">HEX: #BAF1B5</p>
+              <p class="text-brand-darker opacity-75">RGB: 186, 241, 181</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darker bg-brand-lighter rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darker my-auto">Melon</p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darker opacity-75">HEX: #D9FFD7</p>
+              <p class="text-brand-darker opacity-75">RGB: 204, 255, 201</p>
+            </div>
+          </div>
+          <div
+            class="flex xl:flex-row flex-col text-center xl:text-left justify-between p-4 border-2 border-brand-darkest bg-brand-lightest rounded-xl"
+          >
+            <p class="font-display text-xl text-brand-darkest my-auto">
+              Mint Salju
+            </p>
+            <div
+              class="flex flex-col text-sm text-center xl:text-right my-auto"
+            >
+              <p class="text-brand-darkest opacity-75">HEX: #EBFFEA</p>
+              <p class="text-brand-darkest opacity-75">RGB: 217, 255, 215</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">Font</h1>
+
+        <p>
+          Untuk web dan cetak, kami menggunakan Grandstander untuk judul, dan
+          Montserrat untuk badan.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4">
+        <h1 class="text-2xl font-medium dark:text-brand-light">
+          Barang Dagangan
+        </h1>
+
+        <p>
+          Kami tidak memperbolehkan penggunakan merek dagang kami untuk kaos,
+          stiker, atau barang dagangan lainnya tanpa izin tertulis yang jelas.
+        </p>
+      </div>
+      <div class="flex flex-col gap-y-4 mb-16">
+        <h1 class="text-2xl font-medium dark:text-brand-light">
+          Informasi Tambahan
+        </h1>
+
+        <p>
+          Aset merek resmi kami dilisensikan di bawah
+          <nuxt-link
+            class="hover:underline dark:hover:text-brand-lightest hover:text-brand-darkest"
+            to="/id/redirect?url=https://github.com/pnxl/senangsegar/blob/main/LICENSE"
+            >Creative Commons BY-NC-ND 4.0</nuxt-link
+          >.
+        </p>
+
+        <p>
+          Untuk informasi lebih lanjut mengenai penggunaan nama, merek, dan
+          merek dagang, silakan kirim email ke
+          <a
+            class="hover:underline dark:hover:text-brand-lightest hover:text-brand-darkest"
+            href="mailto:brand@senangsegar.xyz"
+            >brand@senangsegar.xyz</a
+          >.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
