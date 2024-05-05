3a87cd44d7cb6901242d60f253602307e1ad7e11
Jason <development@pnxl.dev>
2024-05-05 13:05:37 +0700

    feat: add about

--- pages/en/index.vue
+++ pages/en/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen min-w-full flex flex-col gap-y-4">
-    <div class="flex flex-col gap-y-8">
+  <div class="min-h-screen min-w-full flex flex-col gap-y-32 mb-16">
+    <div class="flex flex-col gap-y-8 h-screen">
       <h1
         class="font-display text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium lg:w-2/3 dark:text-brand-light"
       >
@@ -16,6 +16,28 @@
       >
         <p>See the menu</p>
       </nuxt-link> -->
+    </div>
+    <div class="flex flex-col gap-y-8 -mt-80 pt-48" id="about-us">
+      <h1
+        class="font-display text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium dark:text-brand-light"
+      >
+        About Us
+      </h1>
+      <div class="flex flex-col gap-y-4">
+        <p class="md:w-2/3">
+          Senang Segar is inspired by the products it offers, namely drinks
+          cold, designed to provide freshness and happiness to consumers.
+        </p>
+        <p class="md:w-2/3">
+          We want to delight and refresh customers, with variety cold drinks
+          such as mixed ice, fruit soda, and smoothies.
+        </p>
+        <p class="md:w-2/3">
+          Our strategy involves placing stands in strategic locations, to
+          refresh the students when they are tired after physical activities, so
+          that they are enthusiastic about learning again.
+        </p>
+      </div>
     </div>
   </div>
 </template>
--- pages/id/index.vue
+++ pages/id/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen min-w-full flex flex-col gap-y-4">
-    <div class="flex flex-col gap-y-8">
+  <div class="min-h-screen min-w-full flex flex-col gap-y-32 mb-16">
+    <div class="flex flex-col gap-y-8 h-screen">
       <h1
         class="font-display text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium lg:w-2/3 dark:text-brand-light"
       >
@@ -16,6 +16,29 @@
       >
         <p>Lihat Menu</p>
       </nuxt-link> -->
+    </div>
+    <div class="flex flex-col gap-y-8 -mt-80 pt-48" id="about-us">
+      <h1
+        class="font-display text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium dark:text-brand-light"
+      >
+        Tentang Kita
+      </h1>
+      <div class="flex flex-col gap-y-4">
+        <p class="md:w-2/3">
+          Senang Segar terinspirasi dari produk yang ditawarkan, yaitu minuman
+          dingin, yang dirancang untuk memberikan kesegaran dan kebahagiaan
+          kepada konsumen.
+        </p>
+        <p class="md:w-2/3">
+          Kami ingin menyenangkan dan menyegarkan pelanggan, dengan berbagai
+          minuman dingin seperti es campur, soda buah, dan smoothie.
+        </p>
+        <p class="md:w-2/3">
+          Strategi penjualan melibatkan penempatan stand di lokasi strategis,
+          untuk menyegarkan para siswa saat mereka lelah setelah kegiatan fisik,
+          sehingga mereka kembali semangat untuk belajar.
+        </p>
+      </div>
     </div>
   </div>
 </template>
