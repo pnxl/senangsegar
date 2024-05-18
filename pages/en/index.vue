1938b47f66d0bf453fe0b0fafc15ae69eefc367b
Jason <development@pnxl.dev>
2024-05-18 14:47:45 +0700

    chore: update subheading

--- pages/en/index.vue
+++ pages/en/index.vue
@@ -7,8 +7,8 @@
         Drinks that cool you down more than a cucumber.
       </h1>
       <p class="text-lg md:w-1/2 dark:text-brand-dark">
-        We serve freshly made smoothies, fruit sodas, and es campur - great for
-        those hot summers!
+        We serve freshly made smoothies and es cendol - great for those hot
+        summers!
       </p>
       <nuxt-link
         to="/en/menu"
